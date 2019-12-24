07c49c77354f26b694627bfd7127eb596d7ecdf7
Trey Dockendorf <treydock@gmail.com>
2019-12-24 15:42:28 -0500

    More integration with management of desktop and amazon devices

---
+++ scripts/desktop-active.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ping -c 2 -W 2 192.168.68.105 2>/dev/null 1>/dev/null
+if [ $? -ne 0 ]; then
+  echo "0"
+  exit 0
+fi
+
+STATUS=$(ssh treydock@192.168.68.105 /home/treydock/bin/desktop-active.sh)
+if [ "$STATUS" = "1" ]; then
+  echo "1"
+  exit 0
+fi
+
+echo "0"
+exit 0
---
+++ scripts/desktop-ssh-connections.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ping -c 2 -W 2 -q 192.168.68.105 2>/dev/null 1>/dev/null
+if [ $? -ne 0 ]; then
+  echo "0"
+  exit 0
+fi
+
+COUNT=$(ssh treydock@192.168.68.105 "ss | grep -v '192.168.68.127' | grep -c ssh")
+if [ $COUNT -gt 0 ]; then
+  echo $COUNT
+  exit 0
+fi
+
+echo "0"
+exit 0
+
