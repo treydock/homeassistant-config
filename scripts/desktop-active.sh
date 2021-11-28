b10fcbc77ac5c746cddde651ec4f160ce15ba6c0
Trey Dockendorf <treydock@gmail.com>
2021-11-28 10:35:23 -0500

    Disable hostkey checks for home network SSH

--- scripts/desktop-active.sh
+++ scripts/desktop-active.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then
   exit 0
 fi
 
-STATUS=$(ssh treydock@192.168.68.105 /home/treydock/bin/desktop-active.sh)
+STATUS=$(ssh -o "StrictHostKeyChecking no" treydock@192.168.68.105 /home/treydock/bin/desktop-active.sh)
 if [ "$STATUS" = "1" ]; then
   echo "1"
   exit 0
